13a0c21fa7d7e034407fe48d42ffed7ee5f27589
User <pcyuen98@gmail.com>
2021-03-02 22:57:54 +0800

    Updates

--- src/main/java/advance/element/queue/PriorityQueueCompare.java
+++ src/main/java/advance/element/queue/PriorityQueueCompare.java
@@ -8,8 +8,8 @@ class PriorityQueueCompare {
 
 		// Creating a priority queue
 		PriorityQueue<Integer> numbers = new PriorityQueue<>(new CustomComparator());
+		numbers.add(0);
 		numbers.add(4);
-		numbers.add(2);
 		numbers.add(1);
 		numbers.add(3);
 		System.out.print("PriorityQueue: " + numbers);
@@ -20,6 +20,9 @@ class CustomComparator implements Comparator<Integer> {
 
 	@Override
 	public int compare(Integer number1, Integer number2) {
+		
+		System.out.println("\n--->number1: " + number1);
+		System.out.println("--->number2: " + number2);
 		int value = number1.compareTo(number2);
 		// elements are sorted in reverse order
 		if (value > 0) {
