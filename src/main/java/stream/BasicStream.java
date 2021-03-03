14bf9118fd45b1a9c5e30e22059ce59c512a2f5b
User <pcyuen98@gmail.com>
2021-03-03 18:30:06 +0800

    Updates

--- src/main/java/stream/BasicStream.java
+++ src/main/java/stream/BasicStream.java
@@ -3,6 +3,7 @@ package stream;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class BasicStream {
@@ -17,24 +18,44 @@ public class BasicStream {
 
 	public static void filterPerson() {
 
-	        List<Person> persons = Arrays.asList(
-	                new Person("mkyong", 30),
-	                new Person("jack", 20),
-	                new Person("lawrence", 40)
-	        );
+		List<Person> persons = Arrays.asList(new Person("mkyong", 30), new Person("jack", 20),
+				new Person("lawrence", 40));
 
-	        Person result1 = persons.stream()
-	                .filter((p) -> "jack".equals(p.getName()) && 50 == p.getAge())
-	                .findAny()
-	                .orElse(null);
+		Person result1 = persons.stream().filter((p) -> "jack".equals(p.getName()) && 50 == p.getAge()).findAny()
+				.orElse(null);
 
-	        System.out.println("Person exist --->" + (result1 != null ));
+		System.out.println("Person exist --->" + (result1 != null));
 	}
-	
+
+	public static void replace() {
+
+		List<Person> persons = Arrays.asList(new Person("mkyong", 30), new Person("jack", 20),
+				new Person("lawrence", 40));
+		// p.equals("agent") ? "manager" : p.equals("manager") ? "agent" : p
+		
+        List<String> collect = persons.stream().map(x -> x.getName()).collect(Collectors.toList());
+
+	}
+
+	public static void forEach() {
+
+		List<Person> persons = Arrays.asList(new Person("mkyong", 30), new Person("jack", 20),
+				new Person("lawrence", 40));
+
+		persons.stream().forEach(p ->
+
+		{
+			System.out.println("p age-->" + p.getAge());
+			if (p.getAge() == 30) {
+				System.out.println("match");
+			}
+		});
+	}
+
 	public static void filter() {
-		
+
 		System.out.println("\nfilter");
-		
+
 		init();
 
 		result = lines.stream().filter(str -> !"mkyong".equals(str));
@@ -43,32 +64,33 @@ public class BasicStream {
 	}
 
 	public static void findAny() {
-		
+
 		System.out.println("\nfindAny");
-		
+
 		init();
-	
-		Optional<String> result= lines.stream().filter(str -> "mkyong".equals(str)).findAny();
-		
+
+		Optional<String> result = lines.stream().filter(str -> "mkyong".equals(str)).findAny();
+
 		System.out.println("result is present -->" + result.isPresent());
 
 	}
 
 	public static void contains() {
-		
+
 		System.out.println("\ncontains");
-		
+
 		init();
 		boolean isCOntains = lines.contains("mkyong");
-		
+
 		System.out.println("result is contains -->" + isCOntains);
 
 	}
-	
+
 	public static void main(String[] args) {
-		//filter();
-		//findAny();
-		//contains();
-		filterPerson();
+		// filter();
+		// findAny();
+		// contains();
+		// filterPerson();
+		forEach();
 	}
 }
